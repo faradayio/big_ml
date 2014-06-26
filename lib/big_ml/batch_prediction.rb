e8b375ad2593dabb103bac263a4657883071a9cc
Seamus Abshere <seamus@abshere.net>
2014-06-25 23:35:40 -0300

    provide BatchPrediction#download_to_tmp_path using plain net/http

--- lib/big_ml/batch_prediction.rb
+++ lib/big_ml/batch_prediction.rb
@@ -1,5 +1,9 @@
 require 'big_ml/base'
 
+require 'tmpdir'
+require 'uri'
+require 'net/http'
+
 module BigML
   class BatchPrediction < Base
     BATCH_PREDICTION_PROPERTIES = [
@@ -21,10 +25,34 @@ module BigML
         response = client.get("/#{resource_name}/#{id}/download")
         response.body if response.success?
       end
+
+      # standard Net::HTTP usage pulled from http://ruby-doc.org/stdlib-1.9.3/libdoc/net/http/rdoc/Net.html
+      # FIXME this is production only because we're hitting andromeda not dev/andromeda
+      # FIXME quick and dirty tmp path creation
+      def download_to_tmp_path(id)
+        tmp_path = File.join Dir.tmpdir, "#{rand.to_s}.csv"
+        uri = URI("https://bigml.io/andromeda/#{resource_name}/#{id}/download")
+        uri.query = URI.encode_www_form credentials
+        Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+          request = Net::HTTP::Get.new uri.request_uri
+          http.request request do |response|
+            File.open tmp_path, 'w' do |io|
+              response.read_body do |chunk|
+                io.write chunk
+              end
+            end
+          end
+        end
+        tmp_path
+      end
     end
 
     def download
       self.class.download(id)
+    end
+
+    def download_to_tmp_path
+      self.class.download_to_tmp_path(id)
     end
 
   end
